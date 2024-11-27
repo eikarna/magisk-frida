35dd538e2c487c4c4e4d79b6535982f18cf95117
Adnan Syamsafa <137246570+eikarna@users.noreply.github.com>
2024-11-27 13:47:40 +0700

    Update install.sh

--- base/install.sh
+++ base/install.sh
@@ -172,7 +172,7 @@ fi
   ui_print "- IMPORTANT NOTE (MUST READ THIS!!): "
   ui_print "- Due to many detection methods, the frida-server binary renamed to \"hluda\". Please don't think that is suspicious file."
   ui_print "- If you want to run/use, just use -H parameter/connect it to YOUR_ANDROID_IP_ADDRESS:12313 (127.0.0.1 if local). Because the freeda binary listen on 12313 port."
-  ui_print "- If you have any Questions, see https://github.com/eikarna/magisk-strongR-frida"
+  ui_print "- If you have any Questions, visit https://github.com/eikarna/magisk-frida"
 }
 
 # Only some special files require specific permissions
